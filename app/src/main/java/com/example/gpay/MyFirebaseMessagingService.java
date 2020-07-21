03897f6fb9dff5ae692d8e0bd34b702913b283bc
SowmyaAnand <sowmyaanand2509@gmail.com>
2020-07-21 23:59:47 +0530

    integrated the push notification in android

---
+++ app/src/main/java/com/example/gpay/MyFirebaseMessagingService.java
@@ -0,0 +1,18 @@
+package com.example.gpay;
+
+import androidx.annotation.NonNull;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+    @Override
+    public void onMessageReceived(@NonNull RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
+        if(remoteMessage.getNotification()!=null)
+        {
+            String title = remoteMessage.getNotification().getTitle();
+            String text = remoteMessage.getNotification().getBody();
+        }
+    }
+}
---
+++ app/src/main/java/com/example/gpay/NotificationHelper.java
@@ -0,0 +1,13 @@
+package com.example.gpay;
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.graphics.Color;
+import android.os.Build;
+
+public class NotificationHelper
+{
+
+}
